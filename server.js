398e50d8e6d642c26c1ac2a0d9374e8d9d87d47a
anuj <anuj342466@gmail.com>
2025-08-27 12:33:14 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json({
+        status:200,
+        message:"Hello World"           
+    })
+})
+
+app.listen(8080, () => {    
+    console.log('Server is running on http://localhost:8080')
+})
